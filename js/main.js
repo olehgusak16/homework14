5551b987d37317aaaecccb8c405ad7b0ab765c96
olehgusak16 <olehgusak16@gmail.com>
2023-10-19 23:08:17 +0300

    done homework 14

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let firstNumber = +prompt('Введіть 1-ше число:');
+let secondNumber = +prompt('Введіть 2-ге число:');
+let thirdNumber = +prompt('Введіть 3-те число:');
+
+alert(`Середнє значення чисел ${firstNumber}, ${secondNumber}, ${thirdNumber} = ${(firstNumber + secondNumber + thirdNumber)/3}`);
